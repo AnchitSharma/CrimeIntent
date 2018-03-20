6f9c147cc592a2ba552026d0c4bd5320041196d8
anchitj2ee <anchitj2ee@gmail.com>
2018-03-20 22:30:58 +0530

    Filter Operator

--- app/src/main/java/com/anchitsharma/user/gpstracking/rxjava/MainActivity.java
+++ app/src/main/java/com/anchitsharma/user/gpstracking/rxjava/MainActivity.java
@@ -29,35 +29,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       Observable.just(1,2,3,4,5,6,7,9)
-                .filter(new Predicate<Integer>() {
-                    @Override
-                    public boolean test(@NonNull Integer integer) throws Exception {
-                       return integer%2==0;
-                    }
-                }).subscribe(new DisposableObserver<Integer>() {
-                    @Override
-                    public void onNext(@NonNull Integer integer) {
-                        Log.d(TAG, "onNext: "+integer);
-                    }
+        Observable<User> userObservable = getObservable();
+       userObservable
+               .subscribeOn(Schedulers.io())
+               .observeOn(AndroidSchedulers.mainThread())
+               .filter(new Predicate<User>() {
+                   @Override
+                   public boolean test(@NonNull User user) throws Exception {
+                       return user.getGender().equalsIgnoreCase("female");
+                   }
+               })
+               .subscribeWith(new DisposableObserver<User>() {
+                   @Override
+                   public void onNext(@NonNull User user) {
+                       Log.d(TAG, user.getUname()+"="+user.getGender());
+                   }
 
-                    @Override
-                    public void onError(@NonNull Throwable e) {
+                   @Override
+                   public void onError(@NonNull Throwable e) {
 
-                    }
+                   }
 
-                    @Override
-                    public void onComplete() {
+                   @Override
+                   public void onComplete() {
 
-                    }
-                });
+                   }
+               });
 
 
     }
 
 
 
+private Observable<User> getObservable(){
+    final List<User> users = new ArrayList<>();
+    users.add(new User("Mark","male"));
+    users.add(new User("Lucy","female"));
+    users.add(new User("John","male"));
+    users.add(new User("Scarlett","female"));
+    users.add(new User("Trump","male"));
+    users.add(new User("April","female"));
+    users.add(new User("Obama","male"));
+    return Observable.create(new ObservableOnSubscribe<User>() {
+        @Override
+        public void subscribe(@NonNull ObservableEmitter<User> emitter) throws Exception {
+            for (User user: users){
+                if(!emitter.isDisposed()){
+                    emitter.onNext(user);
+                }
+            }
 
+            if (!emitter.isDisposed()){
+                emitter.onComplete();
+            }
+        }
+    });
+}
 
 
 
