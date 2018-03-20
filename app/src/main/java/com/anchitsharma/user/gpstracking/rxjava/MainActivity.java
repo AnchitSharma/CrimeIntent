71530d75af3bd93cf3c4ba15eb71e0c559acac81
anchitj2ee <anchitj2ee@gmail.com>
2018-03-20 22:18:10 +0530

    RxJava Examples1

--- app/src/main/java/com/anchitsharma/user/gpstracking/rxjava/MainActivity.java
+++ app/src/main/java/com/anchitsharma/user/gpstracking/rxjava/MainActivity.java
@@ -16,83 +16,54 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        compositeDisposable.add(
-                getNotesObservable().subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .map(new Function<Note, Note>() {
-                            @Override
-                            public Note apply(@NonNull Note note) throws Exception {
-                                note.setNote(note.getNote().toUpperCase());
-                                return note;
-                            }
-                        }).subscribeWith(getNotesObserver())
-        );
 
-
-    }
-
-    private DisposableObserver<Note> getNotesObserver() {
-        return new DisposableObserver<Note>() {
-            @Override
-            public void onNext(@NonNull Note note) {
-                Log.d(TAG, "onNext: " + note.getNote());
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-                Log.e(TAG, "onError: " + e.getMessage());
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: ");
-            }
-        };
-    }
-
-    private Observable<Note> getNotesObservable() {
-        final List<Note> notes = prepareNotes();
-        return Observable.create(new ObservableOnSubscribe<Note>() {
-            @Override
-            public void subscribe(@NonNull ObservableEmitter<Note> emitter) throws Exception {
-                for (Note note : notes) {
-                    if (!emitter.isDisposed()) {
-                        emitter.onNext(note);
+       Observable.just(1,2,3,4,5,6,7,9)
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(@NonNull Integer integer) throws Exception {
+                       return integer%2==0;
                     }
-                }
+                }).subscribe(new DisposableObserver<Integer>() {
+                    @Override
+                    public void onNext(@NonNull Integer integer) {
+                        Log.d(TAG, "onNext: "+integer);
+                    }
 
-                if (!emitter.isDisposed()) {
-                    emitter.onComplete();
-                }
-            }
-        });
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
     }
 
-    private List<Note> prepareNotes() {
-        List<Note> notes = new ArrayList<>();
-        notes.add(new Note(1, "buy tooth paste!"));
-        notes.add(new Note(2, "call brother!"));
-        notes.add(new Note(3, "watch narcos tonight!"));
-        notes.add(new Note(4, "pay power bill!"));
 
-        return notes;
-    }
+
+
+
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        compositeDisposable.dispose();
+
     }
 }
