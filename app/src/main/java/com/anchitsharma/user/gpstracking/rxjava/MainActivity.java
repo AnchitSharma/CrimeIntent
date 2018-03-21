79e7eb8e48d76df5fe787bff0dbc989a5bce76d1
anchitj2ee <anchitj2ee@gmail.com>
2018-03-21 13:13:54 +0530

    Buffer operator

--- app/src/main/java/com/anchitsharma/user/gpstracking/rxjava/MainActivity.java
+++ app/src/main/java/com/anchitsharma/user/gpstracking/rxjava/MainActivity.java
@@ -3,11 +3,15 @@ package com.anchitsharma.user.gpstracking.rxjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.anchitsharma.user.gpstracking.R;
+import com.jakewharton.rxbinding2.view.RxView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -24,78 +28,64 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-
-
+    private Button btnTapArea;
+    private TextView txtTapResult;
+    private TextView txtTapResultMax;
+    private Disposable disposable;
+    private int maxTaps = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Observable<Note> numberObservable = getObservable();
-      numberObservable
-              .observeOn(Schedulers.io())
-              .subscribeOn(AndroidSchedulers.mainThread())
-              .distinct()
-              .subscribeWith(getObserver());
+        btnTapArea = (Button)findViewById(R.id.layout_tap_area);
+        txtTapResult = (TextView)findViewById(R.id.tap_result);
+        txtTapResultMax = (TextView)findViewById(R.id.tap_result_max_count);
+
+        RxView.clicks(btnTapArea)
+                .map(new Function<Object, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Object o) throws Exception {
+                        return 1;
+                    }
+                })
+                .buffer(3, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeOn(Schedulers.io())
+                .subscribeWith(new Observer<List<Integer>>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        disposable = d;
+                    }
+
+                    @Override
+                    public void onNext(@NonNull List<Integer> integers) {
+                        Log.d(TAG, "onNext: "+integers.size()+" taps received");
+                        for(int i :integers){
+                            maxTaps = integers.size()>maxTaps?integers.size():maxTaps;
+                            txtTapResult.setText(String.format("Received %d taps in 3 seconds",integers.size()));
+                            txtTapResultMax.setText(String.format("Maximum of %d taps recieved in this session ", maxTaps));
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.d(TAG, "onError: "+e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete: ");
+                    }
+                });
 
 
     }
-
-
-
-
-
-
-    private DisposableObserver<Note> getObserver(){
-        return new DisposableObserver<Note>() {
-            @Override
-            public void onNext(@NonNull Note note) {
-                Log.d(TAG, "onNext: "+note.getNote());
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-                Log.d(TAG, "onError: "+e);
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: ");
-            }
-        };
-    }
-
-private Observable<Note> getObservable(){
-    final List<Note> notes = new ArrayList<>();
-    notes.add(new Note(1, "Buy tooth paste!"));
-    notes.add(new Note(2, "Call brother!"));
-    notes.add(new Note(3, "Call brother!"));
-    notes.add(new Note(4, "Pay power bill!"));
-    notes.add(new Note(5, "Watch Narcos tonight!"));
-    notes.add(new Note(6, "Buy tooth paste!"));
-    notes.add(new Note(7, "Pay power bill!"));
-
-    return Observable.create(new ObservableOnSubscribe<Note>() {
-        @Override
-        public void subscribe(@NonNull ObservableEmitter<Note> emitter) throws Exception {
-            for(Note note:notes){
-                if (!emitter.isDisposed()){
-                    emitter.onNext(note);
-                }
-            }
-
-            if (!emitter.isDisposed()){
-                emitter.onComplete();
-            }
-
-        }
-    });
-}
-
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        disposable.dispose();
 
     }
 }
